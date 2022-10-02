cea78e73fa3d710d252e20cdb97529ac8ca368f3
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-10-02 16:14:17 +0300

    Render bots after first load

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,16 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({bots}) {
   // Your code here
   return (
     <div className="ui four column grid">
-      <div className="row">
+      <div className="row" style={{padding: "10px"}}>
         {/*...and here..*/}
-        Collection of all bots
+        
+        {bots.map((bot)=> <BotCard key={bot.id} bot={bot}/>
+        )}
+
       </div>
     </div>
   );
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  //set state
+  const[bots, setBots] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:8002/bots")
+    .then((r)=>(r.json()))
+    .then((bots)=>setBots(bots))
+  }, [])
+
+  if(!bots) {
+    return <h2>Just a while, we are coming for you</h2>
+  }
 
   return (
     <div>
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection bots={bots}/>
     </div>
   )
 }
